5bcacd536c4f35f96757156f75f154d8b877d4c3
g-shabanin <g.shabanin@gmail.com>
2019-08-23 00:39:13 +0300

    Added article end example "Dependency Injection Using Microsoft Unity Framework" (Saineshwar Bageri)  https://www.c-sharpcorner.com/UploadFile/4d9083/dependency-injection-using-microsoft-unity-framework/

---
+++ Examples/ArticleExample(Saineshwar Bageri)/DIinject/BL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DIinject
+{
+    public class BL
+    {
+        private IProduct _objpro;
+
+        public BL (IProduct objpro)
+        {
+            _objpro = objpro;    
+        }
+
+        public void Insert()
+        {
+            _objpro.Insertdata();
+        }
+
+    }
+}
+
+
+
+
+
---
+++ Examples/ArticleExample(Saineshwar Bageri)/DIinject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace DIinject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Creating microsoft unity Container*/
+            UnityContainer IU = new UnityContainer();
+
+            /* Register a type*/
+              IU.RegisterType<BL>();
+              IU.RegisterType<DL>();
+            /* Register a type with specific members to be injected. */
+
+            IU.RegisterType<IProduct, DL>();
+
+            BL objDL = IU.Resolve<BL>(); 
+            objDL.Insert(); // BL Method
+            Console.ReadLine();
+        }
+    }
+}
